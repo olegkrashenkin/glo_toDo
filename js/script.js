b073f325809080d3bcb058b72fe209192139b0fc
olegkrashenkin <86842418+olegkrashenkin@users.noreply.github.com>
2022-12-20 13:59:47 +0300

    Done

---
+++ js/script.js
@@ -0,0 +1,51 @@
+'use strict'
+
+const todoControl = document.querySelector('.todo-control')
+const headerInput = document.querySelector('.header-input')
+const todoList = document.querySelector('.todo-list')
+const todoCompleted = document.querySelector('.todo-completed')
+let toDo = []
+
+const render = () => {
+    todoList.innerHTML = ''
+    todoCompleted.innerHTML = ''
+    toDo.forEach((item, idx) => {
+        const li = document.createElement('li')
+        li.classList.add('todo-item')
+        li.innerHTML = `<span class="text-todo">${item.text}</span>
+            <div class="todo-buttons">
+            <button class="todo-remove"></button>
+            <button class="todo-complete"></button>
+            </div>`
+        item.completed ? todoCompleted.append(li) : todoList.append(li)
+        li.querySelector('.todo-complete').addEventListener('click', () => {
+            item.completed = !item.completed
+            render()
+        })
+        li.querySelector('.todo-remove').addEventListener('click', () => {
+            toDo.splice(idx, 1)
+            render()
+        })
+    })
+    localStorage.setItem('toDo', JSON.stringify(toDo))
+}
+
+const startApp = () => {
+    const tmp = JSON.parse(localStorage.getItem('toDo'))
+    if (tmp) {
+        toDo = tmp
+        render()
+    }
+}
+
+todoControl.addEventListener('submit', (event) => {
+    event.preventDefault()
+    if (headerInput.value) {
+        const newtoDo = { text: headerInput.value, completed: false }
+        toDo.push(newtoDo)
+        headerInput.value = ''
+        render()
+    }
+})
+
+startApp()
